99d04459b28a5903739aabca39a1878278fa72c6
Kyle-hough <kyle.hough05@gmailcom>
2022-04-29 11:39:16 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var count = 9
+
+function add1(){
+    var countElement = document.querySelector(".count")
+    count++
+    countElement.innerText = count + " like(s)"
+}
+
+var countOne = 12
+
+function add2(){
+    var countElement = document.querySelector(".count-one")
+    countOne++
+    countElement.innerText = countOne + " like(s)"
+}
+
+var countTwo = 9
+
+function add3(){
+    var countElement = document.querySelector(".count-two")
+    countTwo++
+    countElement.innerText = countTwo + " like(s)"
+}
